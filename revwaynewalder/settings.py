0f6aff0a990bddfbaf6649667da36b32066e8b73
Michael Baldanza <michael.snider.baldanza@gmail.com>
2021-10-26 16:12:11 -0400

    cover secret

--- revwaynewalder/settings.py
+++ revwaynewalder/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from secrets import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,9 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#bkmlgqste#w7@9&_4(()k*0#^t1&30q-n84%#f+ix=b$xqdjm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
