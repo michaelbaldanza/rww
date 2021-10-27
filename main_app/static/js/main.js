3d5ffe596e317746ee48f3f2f7f7e01bd704dca2
Michael Baldanza <michael.snider.baldanza@gmail.com>
2021-10-26 20:37:57 -0400

    add auto slider

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,1 +1,17 @@
-console.log('hello')
+let idx = 0;
+
+function changeSlide() {
+  let slides = document.getElementsByClassName('showSlide');
+  if (idx === slides.length - 1) {
+    idx = 0;
+  } else {
+    idx++;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  console.log(idx);
+  console.log(slides[idx]);
+  slides[idx].style.display = 'block';
+}
+let slideChange = setInterval(changeSlide, 5000);
