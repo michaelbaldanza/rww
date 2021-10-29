2941c3f9077c5b15e53369a2b569a9ca51f74349
Michael Baldanza <michael.snider.baldanza@gmail.com>
2021-10-29 16:57:54 -0400

    install django-environ and add to requirements.txt

--- revwaynewalder/settings.py
+++ revwaynewalder/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
+environ.Env()
+environ.Env.read_env()
+
 import os
 import dotenv
 from pathlib import Path
