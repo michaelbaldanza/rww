0946202e6717e51dde61b3ef6a6957d910bf3a80
Michael Baldanza <michael.snider.baldanza@gmail.com>
2022-11-22 16:13:42 -0500

    put stuff back in settings.py

--- revwaynewalder/settings.py
+++ revwaynewalder/settings.py
@@ -151,3 +151,6 @@ AWS_QUERYSTRING_AUTH = False
 
 import django_heroku
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
